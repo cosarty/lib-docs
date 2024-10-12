049f174530d32b108b32578602a083bc14699cc4
cosarty <1398675906@qq.com>
2024-10-13 01:44:53 +0800

    add blog

--- libs/.vitepress/theme/navbar.ts
+++ libs/.vitepress/theme/navbar.ts
@@ -6,6 +6,7 @@ const navbar: DefaultTheme.NavItem[] = [
   { text: '指令', link: '/docs/directives' },
   { text: '样式&动画', link: '/docs/styles' },
   { text: '最佳实践', link: '/docs/practices/storage' },
+  { text: '博客', link: 'https://cosarty.cn/' },
 ];
 
 export default navbar;
